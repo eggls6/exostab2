0f9c22f5752242b1ed316cdb4b9059141e81803b
eggls6 <siegfried.eggl@gmail.com>
2024-03-17 14:47:09 -0500

    more clean-up

---
+++ api/exostab2.py
@@ -0,0 +1,15 @@
+"""
+Exostab 2.0 - Dynamical Stability Assessment for Extra Solar Planets
+"""
+
+__version__ = "0.1.0"
+
+
+import os
+import sys
+
+from core import app
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+
